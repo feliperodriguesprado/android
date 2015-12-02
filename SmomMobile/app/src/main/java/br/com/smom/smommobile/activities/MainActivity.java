88394c9851dc6d8dc67c4a9577a1251cd64dad4b
Diego Nunes <diegodnunes12@gmail.com>
2015-12-02 19:32:37 -0200

    Main Activity or

--- SmomMobile/app/src/main/java/br/com/smom/smommobile/activities/MainActivity.java
+++ SmomMobile/app/src/main/java/br/com/smom/smommobile/activities/MainActivity.java
@@ -2,46 +2,29 @@ package br.com.smom.smommobile.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import br.com.smom.smommobile.R;
 import br.com.smom.smommobile.controllers.CustomerController;
-import br.com.smom.smommobile.dao.CustomerDAOWS;
-import br.com.smom.smommobile.entities.CustomerEntity;
+import br.com.smom.smommobile.controllers.ReleasesController;
 
 public class MainActivity extends Activity {
 
     private CustomerController customerController;
+    private ReleasesController releasesController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        customerController = new CustomerController(getApplicationContext());
-        List<CustomerEntity> customerList = customerController.getCustomerList();
-
-        for (CustomerEntity customer : customerList) {
-            System.out.println(customer.getId());
-            System.out.println(customer.getName());
-            System.out.println(customer.getCpfCnpj());
-        }
-
         /*ListAdapterItem listaAdapterItem = new ListAdapterItem(this, customerList);
 
         ListView listView = (ListView) findViewById(R.id.listViewCustumers);
 
         listView.setAdapter(listaAdapterItem);*/
+
     }
 
     @Override
